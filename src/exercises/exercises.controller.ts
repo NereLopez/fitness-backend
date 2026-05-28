bf618b21ae4a100f2745d4edc4a805d173d268c6
Nere Lopez <nlopez@pav-online.com>
2026-05-28 12:11:12 +0200

    Fix:añadido exercices

--- src/exercises/exercises.controller.ts
+++ src/exercises/exercises.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { ExercisesService } from './exercises.service';
 import { CreateExerciseDto } from './dto/create-exercise.dto';
 import { UpdateExerciseDto } from './dto/update-exercise.dto';
@@ -18,17 +18,17 @@ export class ExercisesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.exercisesService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.exercisesService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateExerciseDto: UpdateExerciseDto) {
-    return this.exercisesService.update(+id, updateExerciseDto);
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateExerciseDto: UpdateExerciseDto) {
+    return this.exercisesService.update(id, updateExerciseDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.exercisesService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.exercisesService.remove(id);
   }
 }
